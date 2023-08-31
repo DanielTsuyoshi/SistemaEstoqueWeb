71e02452754b77099ec2e324d8ca582ec1f1270f
DanielTsuyoshi <110398603+DanielTsuyoshi@users.noreply.github.com>
2023-08-31 18:48:35 -0300

    Add files via upload

---
+++ src/app/itens/DataRow.jsx
@@ -0,0 +1,17 @@
+import DropMenu from "@/components/DropMenu";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+
+export default function DataRow({item}){
+    return (
+        <div id="data-row" className="flex items-center justify-between hover:bg-slate-800 p-2 rounded cursor-pointer">
+            <div className="flex gap-2">
+              <ShoppingCartIcon className="h-6 w-6" />
+              <span>{item.nome}</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <span>R$148.00</span>
+              <DropMenu />
+            </div>
+        </div>
+    )
+}
--- src/app/itens/page.js
+++ src/app/itens/page.js
@@ -1,13 +1,24 @@
 import NavBar from "@/components/NavBar";
+import DataRow from "./DataRow";
 
+async function getItens(){
+  const url = "http://localhost:8080/api/itens"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
 
-export default function Itens() {
+export default async function Itens() {
+  const data = await getItens()
+
   return (
     <>
       <NavBar active={"itens"} />
 
       <main className="bg-slate-900 m-12 p-4 rounded">
-        <h2>Itens</h2>
+        <h2 className="text-2xl font-bold">Itens</h2>
+        <div id="data">
+          {data.map(item => <DataRow item={item} />)}
+        </div>
       </main>
     </>
   )
